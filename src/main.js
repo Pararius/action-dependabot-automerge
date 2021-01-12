5fb6615391aea4c5c48d642d1680081eec327139
pkruithof <pkruithof@gmail.com>
2021-01-12 16:19:12 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -20,16 +20,17 @@ async function run() {
   const requestMerge = core.getInput("request-merge") || "true";
   const token = core.getInput("token") || process.env.GITHUB_TOKEN;
 
-  core.info('debug env vars:');
-  for (let envKey in process.env) {
-    core.info(`${envKey}=${process.env[envKey]}`);
-  }
-
   if (!token) {
-    throw new Error(
-      "No GitHub token is supplied, add \"token\" to your configuration " +
-      "or set the `GITHUB_TOKEN` environment variable"
-    );
+    core.info('debug env vars:');
+    for (let envKey in process.env) {
+      core.info(`${envKey}=${process.env[envKey]}`);
+    }
+    core.error("no token specified");
+    return;
+    // throw new Error(
+    //   "No GitHub token is supplied, add \"token\" to your configuration " +
+    //   "or set the `GITHUB_TOKEN` environment variable"
+    // );
   }
 
   const pr = new PullRequest(
