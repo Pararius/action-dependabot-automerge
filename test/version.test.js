3cc203ac2d65138f1371d2a10c08b28b2773c449
Peter Kruithof <pkruithof@gmail.com>
2021-01-21 16:34:02 +0100

    Improved semver matching (#7)
    * Improved semver matching
    
    * added more test cases

--- test/version.test.js
+++ test/version.test.js
@@ -23,27 +23,55 @@ describe('extract versions from pull request title', function () {
     )
   });
 
-  it('supports verions without "v" prefix', function () {
+  it('supports versions without "v" prefix', function () {
     assert.deepStrictEqual(
       extractVersions("Bump some/dependency from 1.2.3 to 2.0.0"),
       ["1.2.3", "2.0.0"]
     )
   });
+
+  it('supports incomplete semver versions', function () {
+    assert.deepStrictEqual(
+      extractVersions("Bump some/dependency from v1 to v2"),
+      ["v1", "v2"]
+    );
+    assert.deepStrictEqual(
+      extractVersions("Bump some/dependency from v1.1 to v2.2"),
+      ["v1.1", "v2.2"]
+    );
+  });
 })
 
 describe('check if upgrade matches given level', function () {
   const includes = [
-    ['patch', 'v1.2.0', 'v1.2.1'],
+    ['patch', 'v1.2',        'v1.2.1'],
+    ['patch', 'v1.2.0',      'v1.2.1'],
+    ['patch', 'v1.2.0-beta', 'v1.2.1-beta'],
 
-    ['minor', 'v1.2.0', 'v1.2.1'],
-    ['minor', 'v1.2.0', 'v1.3.0'],
+    ['minor', 'v1',         'v1.2'],
+    ['minor', 'v1',         'v1.2.1'],
+    ['minor', 'v1.2',       'v1.3'],
+    ['minor', 'v1.2',       'v1.3.0'],
+    ['minor', 'v1.2.0',     'v1.2.1'],
+    ['minor', 'v1.2.0',     'v1.3.0'],
+    ['minor', 'v1.2.0@rc1', 'v1.3.0@rc1'],
 
-    ['major', 'v1.2.0', 'v1.2.1'],
-    ['major', 'v1.2.0', 'v1.3.0'],
-    ['major', 'v1.2.0', 'v2.0.0'],
+    ['major', 'v1',      'v2'],
+    ['major', 'v1',      'v1.2'],
+    ['major', 'v1',      'v1.2.3'],
+    ['major', 'v1.2',    'v1.3.0'],
+    ['major', 'v1.2.0',  'v1.2.1'],
+    ['major', 'v1.2.0',  'v1.3.0'],
+    ['major', 'v1.2.0',  'v2.0.0'],
+    ['major', 'v1@beta', 'v2@beta'],
   ];
 
   const excludes = [
+    ['patch', 'v1',     'v2'],
+    ['patch', 'v1',     'v1.2'],
+    ['patch', 'v1',     'v1.2.0'],
+    ['patch', 'v1.2',   'v1.3'],
+    ['patch', 'v1.2',   'v1.3.0'],
     ['patch', 'v1.2.0', 'v1.2.0'],
     ['patch', 'v1.2.0', 'v1.3.0'],
     ['patch', 'v1.2.0', 'v2.0.0'],
@@ -52,17 +80,24 @@ describe('check if upgrade matches given level', function () {
     ['patch', 'v1.2.0', 'v1.2.1-beta'],
     ['patch', 'v1.2.0', 'v1.3.0-beta'],
     ['patch', 'v1.2.0', 'v2.0.0-beta'],
+    ['patch', 'v1.2.0-alpha1', 'v1.2.0-alpha2'],
 
+    ['minor', 'v1',     'v2'],
+    ['minor', 'v1',     'v2.0'],
+    ['minor', 'v1',     'v2.0.0'],
+    ['minor', 'v1.2',   'v2.0.0'],
     ['minor', 'v1.2.0', 'v2.0.0'],
     ['minor', 'v1.2.0', 'v1.1.0'],
     ['minor', 'v1.2.0', 'v0.9.0'],
     ['minor', 'v1.2.0', 'v1.3.0-beta'],
     ['minor', 'v1.2.0', 'v2.0.0-beta'],
+    ['minor', 'v1.2.0-alpha1', 'v1.2.0-alpha2'],
 
     ['major', 'v1.2.0', 'v1.1.0'],
     ['major', 'v1.2.0', 'v0.9.0'],
     ['major', 'v1.2.0', 'v1.3.0-beta'],
     ['major', 'v1.2.0', 'v2.0.0-beta'],
+    ['major', 'v1.2.0-alpha1', 'v1.2.0-alpha2'],
   ];
 
   includes.forEach(data => {
@@ -75,6 +110,21 @@ describe('check if upgrade matches given level', function () {
     it(`excludes ${data[0]} upgrade for ${data[1]} => ${data[2]}`, function () {
       assert.ok(! levelIncludesUpgrade(data[1], data[2], data[0]));
     })
+  })
+});
+
+describe('supports suffixes that don\'t denote pre-releases', function () {
+  it('extracts versions correctly', function () {
+    assert.deepStrictEqual(
+      extractVersions("Bump node from 15.5.1-alpine to 15.6.0-alpine"),
+      ["15.5.1-alpine", "15.6.0-alpine"]
+    )
+  });
+
+  it('supports upgrades for "<major>.<minor>.<patch>-alpine" formats', function () {
+    assert.ok(levelIncludesUpgrade("15.5.1-alpine", "15.5.2-alpine", "patch"));
+    assert.ok(levelIncludesUpgrade("15.5.1-alpine", "15.6.0-alpine", "minor"));
+    assert.ok(levelIncludesUpgrade("15.5.1-alpine", "16.0.0-alpine", "major"));
   })
 });
 
